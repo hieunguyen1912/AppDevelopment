34882e1d4a03ad2e3ec8bd1613ca5b312ed86276
HieuNguyeen <hn151331@gmail.com>
2025-01-06 15:24:20 +0700

    bai thi giua ki

--- Group7Project/Ecommerceapp/tmdt_app/lib/main.dart
+++ Group7Project/Ecommerceapp/tmdt_app/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
+class User{
+  String username;
+  String password;
+  String role;
+
+  User({
+    required this.username, required this.password, required this.role
+  });
+}
+void main() {  
   runApp(const MyApp());
 }
 
@@ -14,31 +23,67 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _UserGridState createState() => _UserGridState();
+}
+
+class _UserGridState extends State<HomePage> {
+
+  List<User> users = [
+    User(username: 'Nguyen Trung Hieu', password: '1234', role: 'guest'),
+    User(username: 'Do Xuan Ha', password: '123456', role: 'guest'),
+    User(username: 'Bui Hai Ha', password: '9876', role: 'guest'),
+    User(username: 'Nguyen Tuan Minh', password: '1010', role: 'admin'),
+    User(username: 'Bui Nam anh', password: '4567', role:'admin'),
+  ]; 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Trang chủ'),
+        title: Text('Trang chủ'),
         centerTitle: true,
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.search))
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 50,
-            color: Colors.blue[200],
-          )
-        ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 2,
+        ),
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return Card(
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Username: ${user.username}',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text('Role: ${user.role}', style: TextStyle(fontSize: 14),)
+                ],
+              )
+            ),
+          );
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
