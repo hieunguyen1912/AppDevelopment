ad4c4451432912bb4787a3410be0121e84167784
HieuNguyeen <hn151331@gmail.com>
2025-01-14 21:06:06 +0700

    set up assets and add models, views, controllers folder

--- Group7Project/Ecommerceapp/tmdt_app/lib/main.dart
+++ Group7Project/Ecommerceapp/tmdt_app/lib/main.dart
@@ -32,13 +32,96 @@ class HomePage extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.search))
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 50,
-            color: Colors.blue[200],
-          )
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Tiêu đề
+                  Text(
+                    'Strawberry Pavlova',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. '
+                    'Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.',
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.star, color: Colors.red, size: 10.0),
+                            Icon(Icons.star, color: Colors.red, size: 10.0),
+                            Icon(Icons.star, color: Colors.red, size: 10.0),
+                            Icon(Icons.star, color: Colors.red, size: 10.0),
+                            Icon(Icons.star_half, color: Colors.red, size: 10.0),
+                          ],
+                        ),
+                        Flexible(
+                          child: Text(
+                            '170 reviews',
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.timer, size: 15.0,),
+                          SizedBox(height: 2),
+                          Text('PREP:',  style: TextStyle(fontSize: 10)),
+                          Text('25 min',  style: TextStyle(fontSize: 10)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.restaurant, size: 15.0,),
+                          SizedBox(height: 2),
+                          Text('COOK:',  style: TextStyle(fontSize: 10)),
+                          Text('1 hr',  style: TextStyle(fontSize: 10)),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.people,size: 15.0,),
+                          SizedBox(height: 2),
+                          Text('FEEDS:', style: TextStyle(fontSize: 10)),
+                          Text('4-6', style: TextStyle(fontSize: 10)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Image.asset(
+                'assets/shoe.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
