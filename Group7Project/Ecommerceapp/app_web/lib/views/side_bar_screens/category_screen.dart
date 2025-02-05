61d88fbf1f35c8468783f5374dbbd2a830a7510d
Le Dang <leedang2112@gmail.com>
2025-02-06 01:14:35 +0700

    Validator Check

--- Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/category_screen.dart
+++ Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/category_screen.dart
@@ -1,94 +1,118 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   static const String id = '/categoryScreen';
   const CategoryScreen({super.key});
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Categories',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Categories',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        Divider(
-          color: Colors.grey,
-        ),
-        Row(
-          children: [
-            Column(
-              children: [
-                Container(
-                  height: 140,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade500,
-                    border: Border.all(color: Colors.grey.shade800),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Upload Image ',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 140,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade500,
+                      border: Border.all(color: Colors.grey.shade800),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Upload Image ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Upload Image',
-                      style: TextStyle(
-                        color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Upload Image',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Container(
-              width: 150,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Category Name',
-                ),
+                ],
               ),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                  Colors.white,
-                ),
-                side: MaterialStateProperty.all(
-                  BorderSide(
-                    color: Colors.blue.shade900,
+              SizedBox(
+                width: 30,
+              ),
+              SizedBox(
+                width: 150,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a category name';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Category Name',
                   ),
                 ),
               ),
-              onPressed: () {},
-              child: Text(
-                "Save",
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.white,
+                  ),
+                  side: MaterialStateProperty.all(
+                    BorderSide(
+                      color: Colors.blue.shade900,
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    //upload calegory firebase
+                  } else {
+                    //show erropr message
+                    print('bad reponse');
+                  }
+                },
+                child: const Text(
+                  "Save",
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
