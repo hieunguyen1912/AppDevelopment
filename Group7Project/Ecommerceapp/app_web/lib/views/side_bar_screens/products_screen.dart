ce96d52312244fa360659e41f39f4797211a83dc
Le Dang <leedang2112@gmail.com>
2025-02-10 02:50:21 +0700

    Create Size Button

--- Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/products_screen.dart
+++ Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/products_screen.dart
@@ -18,7 +18,9 @@ class _ProductsScreenState extends State<ProductsScreen> {
   final List<String> _categoryList = [];
   String? selectedCategory;
 
-  List<Uint8List> _images = [];
+  bool _isEntered = false;
+
+  final List<Uint8List> _images = [];
 
   chooseImage() async {
     final pickedImages = await FilePicker.platform.pickFiles(
@@ -58,141 +60,168 @@ class _ProductsScreenState extends State<ProductsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 400,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Products Information',
-            style: TextStyle(
-              fontSize: 19,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Enter Product Name',
-              filled: true,
-              fillColor: Colors.grey[200],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Products Information',
+              style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter Price',
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+            SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Product Name',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Price',
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Flexible(
+                  child: buildDropDownField(),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Discount Price',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Discription',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _isEntered = true;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Add size',
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                _isEntered == true
+                    ? Flexible(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Add'),
+                        ),
+                      )
+                    : const Text(''),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GridView.builder(
+                itemCount: _images.length + 1,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8),
+                itemBuilder: (context, index) {
+                  return index == 0
+                      ? Center(
+                          child: IconButton(
+                              onPressed: () {
+                                chooseImage();
+                              },
+                              icon: const Icon(Icons.add)),
+                        )
+                      : Image.memory(_images[index - 1]);
+                }),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                width: MediaQuery.of(context).size.width * 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Upload Product',
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                width: 20,
-              ),
-              Flexible(
-                child: buildDropDownField(),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Discount Price',
-              filled: true,
-              fillColor: Colors.grey[200],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Enter Discription',
-              filled: true,
-              fillColor: Colors.grey[200],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Add size',
-              filled: true,
-              fillColor: Colors.grey[200],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide.none,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          GridView.builder(
-              itemCount: _images.length + 1,
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8),
-              itemBuilder: (context, index) {
-                return index == 0
-                    ? Center(
-                        child: IconButton(
-                            onPressed: () {
-                              chooseImage();
-                            },
-                            icon: const Icon(Icons.add)),
-                      )
-                    : Image.memory(_images[index - 1]);
-              }),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              width: MediaQuery.of(context).size.width * 50,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  'Upload Product',
-                  style: TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
