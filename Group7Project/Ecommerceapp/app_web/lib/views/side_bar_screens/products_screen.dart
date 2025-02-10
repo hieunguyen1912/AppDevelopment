d880c7e99a2d0c7722878da97bee6ad752614642
Le Dang <leedang2112@gmail.com>
2025-02-10 11:36:50 +0700

    Form Validation

--- Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/products_screen.dart
+++ Group7Project/Ecommerceapp/app_web/lib/views/side_bar_screens/products_screen.dart
@@ -16,6 +16,7 @@ class ProductsScreen extends StatefulWidget {
 class _ProductsScreenState extends State<ProductsScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _sizeController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   final List<String> _categoryList = [];
 
@@ -67,101 +68,55 @@ class _ProductsScreenState extends State<ProductsScreen> {
     return SingleChildScrollView(
       child: SizedBox(
         width: 400,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Products Information',
-              style: TextStyle(
-                fontSize: 19,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Product Name',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Products Information',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter Price',
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter filed';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter Product Name',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
                   ),
                 ),
-                SizedBox(
-                  width: 20,
-                ),
-                Flexible(
-                  child: buildDropDownField(),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Discount Price',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Discription',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: SizedBox(
-                    width: 200,
+              Row(
+                children: [
+                  Flexible(
                     child: TextFormField(
-                      controller: _sizeController,
-                      onChanged: (value) {
-                        setState(() {
-                          _isEntered = true;
-                        });
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter filed';
+                        } else {
+                          return null;
+                        }
                       },
                       decoration: InputDecoration(
-                        labelText: 'Add size',
+                        labelText: 'Enter Price',
                         filled: true,
                         fillColor: Colors.grey[200],
                         border: OutlineInputBorder(
@@ -171,107 +126,196 @@ class _ProductsScreenState extends State<ProductsScreen> {
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                _isEntered == true
-                    ? Flexible(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              _sizeList.add(_sizeController.text);
-                              _sizeController.clear();
-                            });
-                          },
-                          child: const Text(
-                            'Add',
-                          ),
-                        ),
-                      )
-                    : const Text(''),
-              ],
-            ),
-            _sizeList.isNotEmpty
-                ? Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SizedBox(
-                      height: 50,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: _sizeList.length,
-                        itemBuilder: (context, index) {
-                          return InkWell(
-                            onTap: () {
-                              setState(() {
-                                _sizeList.removeAt(index);
-                              });
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.blue.shade800,
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    _sizeList[index],
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  )
-                : Text(''),
-            const SizedBox(
-              height: 20,
-            ),
-            GridView.builder(
-                itemCount: _images.length + 1,
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8),
-                itemBuilder: (context, index) {
-                  return index == 0
-                      ? Center(
-                          child: IconButton(
-                              onPressed: () {
-                                chooseImage();
-                              },
-                              icon: const Icon(Icons.add)),
-                        )
-                      : Image.memory(_images[index - 1]);
-                }),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                width: MediaQuery.of(context).size.width * 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Text(
-                    'Upload Product',
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Flexible(
+                    child: buildDropDownField(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter filed';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Discount Price',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
                   ),
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                maxLength: 800,
+                maxLines: 4,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter filed';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter Discription',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        controller: _sizeController,
+                        onChanged: (value) {
+                          setState(() {
+                            _isEntered = true;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Add size',
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  _isEntered == true
+                      ? Flexible(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _sizeList.add(_sizeController.text);
+                                _sizeController.clear();
+                              });
+                            },
+                            child: const Text(
+                              'Add',
+                            ),
+                          ),
+                        )
+                      : const Text(''),
+                ],
+              ),
+              _sizeList.isNotEmpty
+                  ? Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        height: 50,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _sizeList.length,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  _sizeList.removeAt(index);
+                                });
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue.shade800,
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      _sizeList[index],
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                  : Text(''),
+              const SizedBox(
+                height: 20,
+              ),
+              GridView.builder(
+                  itemCount: _images.length + 1,
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8),
+                  itemBuilder: (context, index) {
+                    return index == 0
+                        ? Center(
+                            child: IconButton(
+                                onPressed: () {
+                                  chooseImage();
+                                },
+                                icon: const Icon(Icons.add)),
+                          )
+                        : Image.memory(_images[index - 1]);
+                  }),
+              InkWell(
+                onTap: () {
+                  if (_formkey.currentState!.validate()) {
+                    //upload product to cloud firestore
+                    print('upload');
+                  } else {
+                    //please fill in all fieds
+                    print('bad status');
+                  }
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Upload Product',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
